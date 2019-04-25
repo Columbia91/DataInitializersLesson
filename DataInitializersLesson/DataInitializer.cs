bdc32ea0c12ffcf5c8fdb81770dec10e4c795c40
Columbia91 <alpha.z91@mail.ru>
2019-04-25 19:52:13 +0600

    Добавьте файлы проекта.

---
+++ DataInitializersLesson/DataInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInitializersLesson
+{
+    public class DataInitializer : CreateDatabaseIfNotExists<CitiesContext>
+    {
+        protected override void Seed(CitiesContext context)
+        {
+            context.Cities.AddRange(new List<City>
+            {
+                new City
+                {
+                    Name = "Нур-Султан",
+                    Population = 1200000
+                },
+                new City
+                {
+                    Name = "Алматы",
+                    Population = 2000000
+                },
+                new City
+                {
+                    Name = "Стамбул",
+                    Population = 15000000
+                }
+            });
+        }
+    }
+}
---
+++ DataInitializersLesson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInitializersLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new CitiesContext())
+            {
+                context.Cities.Add(new City { Name = "Бишкек", Population = 2000000 });
+                context.SaveChanges();
+
+                foreach (var city in context.Cities.ToList())
+                {
+                    Console.WriteLine(city.Name);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
